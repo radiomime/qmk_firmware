94e37d5a976ef740d72e88ccd53c8bcfe76e6192
Matthew Wright <wright.matthew91@gmail.com>
2021-07-05 21:29:43 -0600

    feat: compile first

--- flash.sh
+++ flash.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# https://beta.docs.qmk.fm/using-qmk/guides/git-best-practices/newbs_git_using_your_master_branch
+
 printf "Flashing Corne with radiomime's keymap\n"
-qmk flash \
+
+qmk compile \
     --keyboard crkbd \
     --keymap radiomime \
-    --bootloader dfu
+
+qmk flash \
+    --keyboard crkbd \
+    --keymap radiomime
