1ac2bfc06cfa637493542510d1c7c68e23a9962b
Matthew Wright <wright.matthew91@gmail.com>
2022-04-17 01:08:00 -0600

    feat: master on left

--- keyboards/crkbd/keymaps/radiomime/config.h
+++ keyboards/crkbd/keymaps/radiomime/config.h
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_RIGHT
-/* #define MASTER_LEFT */
+/* #define MASTER_RIGHT */
+#define MASTER_LEFT
 /* #define EE_HANDS */
 
 /* allow media controls */
