3671d0e9c8f9aa9d2035d2d14f1fccd69789e1f2
Matt Wright <wright.matthew91@gmail.com>
2022-08-21 21:12:50 -0600

    misc: oled work

--- keyboards/crkbd/keymaps/radiomime/rules.mk
+++ keyboards/crkbd/keymaps/radiomime/rules.mk
@@ -1,7 +1,3 @@
-OLED_DRIVER_ENABLE  = yes
-# OLED_DRIVER = SSD1306 # Selects the right driver
-# OLED_ENABLE = yes     # Enables the use of OLED displays
-
 # MCU name
 MCU = atmega32u4
 
@@ -49,6 +45,13 @@ COMBO_ENABLE = yes
 
 # add sources
 SRC += oneshot.c
+
+# oled
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+# OLED_DRIVER_ENABLE  = yes
+# OLED_DRIVER = SSD1306 # Selects the right driver
+# OLED_ENABLE = yes     # Enables the use of OLED displays
 
 # SRC += callum.c
 # SRC += oneshot.c
